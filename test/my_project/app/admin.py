35f0c3bccac5db7037349f727b86c9f78e02471c
ElenaPopD <popelena2689@gmail.com>
2024-01-18 20:23:17 +0200

    improved models

--- test/my_project/app/admin.py
+++ test/my_project/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Produs
+from .models import Produs, Favorit
 
 admin.site.register(Produs)
+admin.site.register(Favorit)
--- test/my_project/app/models.py
+++ test/my_project/app/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Produs(models.Model):
     titlu = models.CharField(max_length=50)
-    pret = models.FloatField()
+    pret = models.FloatField(db_index=True)
     stoc = models.IntegerField(default=0)
-    descriere = models.CharField(max_length=1024, default='No description provided')
-    imagine = models.FileField()
+    descriere = models.CharField(max_length=1024, default='No description provided', null=True, blank=True, help_text="Intoduceti o descriere")
+    imagine = models.FileField(null=True, blank=True)
+
+
+class Favorit(models.Model):
+    produs = models.ForeignKey(Produs, on_delete=models.CASCADE)  # noqa: F821
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
